6f32e1c28a0367d56974aa0eafd598b09b124ca8
Federico P. Cortese <cortese.federico@live.it>
2024-06-07 15:42:17 +0200

    one season only

--- R/ts_imput/run_par_genova.R
+++ R/ts_imput/run_par_genova.R
@@ -75,9 +75,13 @@ diag(round(cor(dat_temp_wide3[,-1],dat_rh_wide3[,-1]),2))
 
 # One year obs
 
-air_short=dat_temp_wide3[1:8760,]
-rh_short=dat_rh_wide3[1:8760,]
+one_year=1:8760
+first_seas=1:2190
 
+# air_short=dat_temp_wide3[1:8760,]
+# rh_short=dat_rh_wide3[1:8760,]
+air_short=dat_temp_wide3[first_seas,]
+rh_short=dat_rh_wide3[first_seas,]
 # 
 # air_short=dat_temp_wide3[1:1000,]
 # rh_short=dat_rh_wide3[1:1000,]
