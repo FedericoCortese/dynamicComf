dc16d03ef0b87e5983a6f78017dacda019d5b1f8
Federico P. Cortese <cortese.federico@live.it>
2024-06-13 09:34:29 +0200

    noth

--- R/SpJM/empirical_study.R
+++ R/SpJM/empirical_study.R
@@ -13,35 +13,26 @@ str(enth_tab)
 
 # space_id, indoor_latitude and indoor_longitude can be used for parsing some environmental features
 # Remove unnecessary columns
-enth_tab=subset(enth_tab,select=-c(building_name,
+enth_tab=subset(enth_tab,select=-c(space_id,
+                                   building_name,
                                    response_speed,
-                                   indoor_floor
-                                   ))
+                                   indoor_floor,
+                                   body_presence,
+                                   user_id,
+                                   indoor_latitude,
+                                   indoor_longitude,
+                                   indoor_floor,
+                                   change
+))
 
 enth_tab$time=as.POSIXct(enth_tab$time,format="%Y-%m-%d %H:%M:%S")
 
-wdn="15 mins"
-enth_tab2=enth_tab%>%
-  group_by(user_id,time=floor_date(time,wdn))%>%
-  summarise_if(is.numeric, mean, na.rm = TRUE) 
+str(enth_tab)
 
-enth01_all=enth_tab[which(enth_tab$user_id=="enth01"),]
-enth02_all=enth_tab[which(enth_tab$user_id=="enth02"),]
-enth03_all=enth_tab[which(enth_tab$user_id=="enth03"),]
-enth04_all=enth_tab[which(enth_tab$user_id=="enth04"),]
-enth05_all=enth_tab[which(enth_tab$user_id=="enth05"),]
-enth07_all=enth_tab[which(enth_tab$user_id=="enth07"),]
-enth09_all=enth_tab[which(enth_tab$user_id=="enth09"),]
-enth10_all=enth_tab[which(enth_tab$user_id=="enth10"),]
-enth11_all=enth_tab[which(enth_tab$user_id=="enth11"),]
-enth13_all=enth_tab[which(enth_tab$user_id=="enth13"),]
-enth15_all=enth_tab[which(enth_tab$user_id=="enth15"),]
-enth16_all=enth_tab[which(enth_tab$user_id=="enth16"),]
-enth17_all=enth_tab[which(enth_tab$user_id=="enth17"),]
-enth20_all=enth_tab[which(enth_tab$user_id=="enth20"),]
-enth22_all=enth_tab[which(enth_tab$user_id=="enth22"),]
-enth25_all=enth_tab[which(enth_tab$user_id=="enth25"),]
-enth28_all=enth_tab[which(enth_tab$user_id=="enth28"),]
+wdn="10 mins"
+enth_tab2=enth_tab%>%
+  group_by(time=floor_date(time,wdn))%>%
+  summarise_if(is.numeric, mean, na.rm = TRUE) 
 
 count_consecutive=function(x,tol=4){
   d_t=diff(x$time)/60
@@ -52,28 +43,33 @@ count_consecutive=function(x,tol=4){
               t=which.max(av)))
 }
 
-count_consecutive(enth01_all)
-count_consecutive(enth02_all)
-count_consecutive(enth03_all)
-count_consecutive(enth04_all)
-count_consecutive(enth05_all)
-count_consecutive(enth07_all)
-count_consecutive(enth09_all)
-count_consecutive(enth10_all)
-count_consecutive(enth11_all)
-count_consecutive(enth13_all)
-count_consecutive(enth15_all)
-count_consecutive(enth16_all)
-count_consecutive(enth17_all)
-count_consecutive(enth20_all)
-count_consecutive(enth22_all)
-count_consecutive(enth25_all)
-count_consecutive(enth28_all)
-
 # "Average" person
-enth_tab_av=enth_tab%>%group_by(time)%>%
+enth_tab_av=enth_tab2%>%group_by(time)%>%
   summarise_if(is.numeric, mean, na.rm = TRUE)
 
-count_consecutive(enth_tab_av)
+cc=count_consecutive(enth_tab_av)
+cbind(cc$av,enth_tab_av$time)
+sort(cc$av)
+cc$max.t
+cc$t
 # Waaaaay better
-enth_tab_av$time[(232-105):231]
+enth_tab_av$time[(156-56):155]
+
+wdn2=(cc$t-1-cc$max.t):cc$t
+
+enth_tab3=enth_tab_av[wdn2,]
+Amelia::missmap(enth_tab3)
+
+str(enth_tab3)
+
+# NAs percentage for each variable
+apply(enth_tab3,2,function(x) sum(is.na(x))/length(x))*100
+
+# Remove vars with 100% NAs
+enth_tab3=subset(enth_tab3,select=-c(co2_indoor,voc_indoor,pm25_indoor,noise_indoor))
+
+enth_tab
+
+plot(enth_tab3$indoor.outdoor)
+
+Amelia::missmap(enth_tab3)
