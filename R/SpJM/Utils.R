12d8d5501b976e95332c7f2d42c4d02b6115f95f
Federico P. Cortese <67327161+FedericoCortese@users.noreply.github.com>
2024-07-10 14:13:50 +0200

    arpal varying K

--- R/SpJM/Utils.R
+++ R/SpJM/Utils.R
@@ -1145,7 +1145,9 @@ jump_mixed2 <- function(Y, n_states, jump_penalty=1e-5,
   return(list(best_s=best_s,
               Y=Y,
               Y.orig=Ynoscaled,
-              condMM=mumo))
+              condMM=mumo,
+              K=n_states,
+              lambda=jump_penalty))
 }
 
 GIC_mixed=function(Y,states,states_sat,K,K0=NULL,pers0=.95,timeflag=F,Ksat=6,lambda=NULL){
