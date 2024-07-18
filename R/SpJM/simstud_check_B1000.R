3ac41dbfbd927381818c22b2c10d6922c8110f75
FedericoCortese <cortese.federico@live.it>
2024-07-18 08:55:43 +0200

    B=1000

--- R/SpJM/simstud_check_B1000.R
+++ R/SpJM/simstud_check_B1000.R
@@ -43,40 +43,46 @@ save(mixedJM_no.miss_setup3,elapsed_no.miss_setup3,file="mixedJM_no_miss_setup3.
 ## SpClust
 library(SpectralClMixed)
 start_no.miss_specluster=Sys.time()
-mixedJM_no.miss_setup1 <- parallel::mclapply(1:nrow(hp),
+mixedJM_specluster_setup1 <- parallel::mclapply(1:nrow(hp),
                                       function(x)
-                                        simstud_JMmixed(
+                                        simstud_speclust(
                                           seed=hp[x,]$seed,
-                                          lambda=hp[x,]$lambda,
                                           TT=hp[x,]$TT,
                                           P=hp[x,]$P,
-                                          Ktrue=3,mu=1,
-                                          phi=.8,rho=0,
-                                          Pcat=NULL,pers=.95,
-                                          pNAs=0,typeNA=2),
+                                          Ktrue=3,
+                                          mu=1,
+                                          phi=.8,
+                                          rho=0,
+                                          Pcat=NULL,
+                                          pers=.95,
+                                          pNAs=0,
+                                          typeNA=2),
                                       mc.cores = parallel::detectCores()-1)
 
 end_no.miss_specluster=Sys.time()
 elapsed_no.miss_specluster_setup1=end_no.miss_specluster-start_no.miss_specluster
-save(mixedJM_no.miss_specluster_setup1,elapsed_no.miss_specluster_setup1,file="mixedJM_no_miss_specluster_setup1.RData")
+save(mixedJM_specluster_setup1,elapsed_no.miss_specluster_setup1,file="mixedJM_no_miss_specluster_setup1.RData")
 
 start_no.miss_specluster=Sys.time()
-mixedJM_no.miss_setup3 <- parallel::mclapply(1:nrow(hp),
+mixedJM_specluster_setup3 <- parallel::mclapply(1:nrow(hp),
                                              function(x)
-                                               simstud_JMmixed(
+                                               simstud_speclust(
                                                  seed=hp[x,]$seed,
-                                                 lambda=hp[x,]$lambda,
                                                  TT=hp[x,]$TT,
                                                  P=hp[x,]$P,
-                                                 Ktrue=3,mu=.5,
-                                                 phi=.8,rho=0,
-                                                 Pcat=NULL,pers=.95,
-                                                 pNAs=0,typeNA=2),
+                                                 Ktrue=3,
+                                                 mu=.5,
+                                                 phi=.8,
+                                                 rho=0,
+                                                 Pcat=NULL,
+                                                 pers=.95,
+                                                 pNAs=0,
+                                                 typeNA=2),
                                              mc.cores = parallel::detectCores()-1)
 
 end_no.miss_specluster=Sys.time()
 elapsed_no.miss_specluster_setup3=end_no.miss_specluster-start_no.miss_specluster
-save(mixedJM_no.miss_specluster_setup3,elapsed_no.miss_specluster_setup3,file="mixedJM_no_miss_specluster_setup3.RData")
+save(mixedJM_specluster_specluster_setup3,elapsed_no.miss_specluster_setup3,file="mixedJM_no_miss_specluster_setup3.RData")
 
 library(dplyr)
 res_eval=function(res_obj,hp,lambda0=F,ARI=T){
@@ -120,7 +126,7 @@ res_eval=function(res_obj,hp,lambda0=F,ARI=T){
 
 res_eval(mixedJM_no.miss_setup1,hp)
 res_eval(mixedJM_no.miss_setup1,hp,lambda0=T)
-res_eval(mixedJM_no.miss_specluster_setup1,hp)
+res_eval(mixedJM_specluster_setup1,hp)
 
 res_eval(mixedJM_no.miss_setup3,hp)
 res_eval(mixedJM_no.miss_setup3,hp,lambda0=T)
