4b05a9b3fc3d1196085368e4c87022ff7bb731f1
Federico P. Cortese <cortese.federico@live.it>
2024-09-03 14:29:59 +0200

    mods

--- R/SpJM/simstud_STJump.R
+++ R/SpJM/simstud_STJump.R
@@ -11,21 +11,25 @@ Pcat=10
 pNAs=0
 
 ###
-lambda=seq(0,0.25,by=.05)
-gamma=seq(0,0.25,by=.05)
+# lambda=seq(0,0.25,by=.05)
+# gamma=seq(0,0.25,by=.05)
+# seed=1:100
+# M=c(25,100)
+# TT=c(10,50)
+
+lambda=.05
+gamma=.05
 seed=1:2
 M=100
 TT=10
 hp=expand.grid(lambda=lambda,gamma=gamma,seed=seed,M=M,TT=TT)
-# seed=1:100
-# M=c(25,100)
-# TT=c(10,50)
+
 
 
 start_STJsim=Sys.time()
 STJsim <- parallel::mclapply(1:nrow(hp),
                              function(x)
-                               STJump_sim(lambda=hp[x,]$lambda,
+                               simstud_STJump(lambda=hp[x,]$lambda,
                                           gamma=hp[x,]$gamma,
                                           seed=hp[x,]$seed,
                                           M=hp[x,]$M,
