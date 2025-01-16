d7417e7bcb129a40037ac97d802a5ce86f83216d
Federico P. Cortese <cortese.federico@live.it>
2025-01-16 17:28:42 +0100

    mods

--- R/SpJM/Utils.R
+++ R/SpJM/Utils.R
@@ -3847,6 +3847,7 @@ STjumpDist=function(Y,n_states,
   # Initialize modes
   mo <- apply(Ycat,2,Mode)
   # Impute missing values with mean of observed states
+  
   for(i in 1:n_cont){
     Ycont[,i]=ifelse(Mcont[,i],mu[i],Ycont[,i])
   }
@@ -3864,7 +3865,217 @@ STjumpDist=function(Y,n_states,
   # State initialization through kmeans++
   S=matrix(0,nrow=TT,ncol=M)
   for(m in 1:M){
-    S[,m]=initialize_states(Y[which(Y$m==m),],n_states)
+    S[,m]=initialize_states(Y[which(Y$m==m),-(1:2)],n_states)
+  }
+  
+  for (init in 1:n_init) {
+    mu <- matrix(0, nrow=n_states, ncol=length(cont.indx))
+    mo <- matrix(0, nrow=n_states, ncol=length(cat.indx))
+    loss_old <- 1e10
+    
+    for (it in 1:max_iter) {
+      for (i in unique(as.vector(S))) {
+        mu[i,] <- colMeans(Ycont[as.vector(t(S))==i,])
+        mo[i,]=apply(Ycat[as.vector(t(S))==i,],2,Mode)
+      }
+      
+      mu=data.frame(mu)
+      mo=data.frame(mo,stringsAsFactors=TRUE)
+      for(i in 1:n_cat){
+        mo[,i]=factor(mo[,i],levels=levels(Ycat[,i]))
+      }
+      mumo=data.frame(matrix(0,nrow=n_states,ncol=P))
+      mumo[,cat.indx]=mo
+      mumo[,cont.indx]=mu
+      colnames(mumo)=colnames(YY)
+      
+      
+      # Fit state sequence
+      S_old <- S
+      
+      loss_v=NULL
+      
+      # Re-fill-in missings 
+      
+      Ycont <- Mcont * mu[as.vector(t(S)), ] + (!Mcont) * Ycont
+      
+      
+      # for(i in 1:nrow(Mcont)){
+      #   Ycont[i,]=unlist(ifelse(Mcont[i,],mu[as.vector(t(S))[i],],Ycont[i,]))
+      # }
+      for(i in 1:nrow(Mcat)){
+        Ycat[i,]=unlist(ifelse(Mcat[i,],mo[as.vector(t(S))[i],],Ycat[i,]))
+      }
+      
+      YY[,-cat.indx]=Ycont
+      YY[,cat.indx]=Ycat
+      
+      Y[,-(1:2)]=YY
+      
+      ###
+      for(m in 1:M){
+        loss_by_state=gower.dist(Y[which(Y$m==m),-(1:2)],mumo)
+        
+        for(k in 1:n_states){
+          #temp <- t(t((S[,-m]==k))/D[m,-m])
+          temp <- t(t((S[,-m]==k))*exp(-D[m,-m]))
+          loss_by_state[,k]=loss_by_state[,k]-spatial_penalty*rowSums(temp)
+        }
+        
+        V <- loss_by_state
+        for (t in (TT-1):1) {
+          #V[t-1,] <- loss_by_state[t-1,] + apply(V[t,] + Gamma, 2, min)
+          if(timeflag){
+            V[t-1,] <- loss_by_state[t-1,] + apply(V[t,]/dtime[t] + Gamma, 2, min)
+          }
+          else{
+            V[t-1,] <- loss_by_state[t-1,] + apply(V[t,] + Gamma, 2, min)
+          }
+        }
+        
+        S[1,m] <- which.min(V[1,])
+        for (t in 2:TT) {
+          S[t,m] <- which.min(V[t,] + Gamma[S[t-1,m],])
+        }
+        loss_v=c(loss_v,min(V[1,]))
+      }
+      if (length(unique(S)) == 1) {
+        break
+      }
+      loss <- mean(loss_v)
+      if (verbose) {
+        cat(sprintf('Iteration %d: %.6e\n', it, loss))
+      }
+      if (!is.null(tol)) {
+        epsilon <- loss_old - loss
+        if (epsilon < tol) {
+          break
+        }
+      } else if (all(S == S_old)) {
+        break
+      }
+      loss_old <- loss
+      
+      ###
+      
+    }
+    if (is.null(best_s) || (loss_old < best_loss)) {
+      best_loss <- loss_old
+      best_s <- S
+    }
+    
+    for(m in 1:M){
+      S[,m]=initialize_states(Y[which(Y$m==m),-(1:2)],n_states)
+    }
+  }
+  return(list(best_s=best_s,
+              Y=Y,
+              K=n_states,
+              lambda=jump_penalty,
+              gamma=spatial_penalty,
+              loss=best_loss))
+  
+}
+
+
+STjumpDist_OLD=function(Y,n_states,
+                    D,
+                    jump_penalty=1e-5,
+                    spatial_penalty=1e-5,
+                    initial_states=NULL,
+                    max_iter=10, n_init=10, tol=NULL, verbose=FALSE,timeflag=T){
+  
+  # This function implements the spatio-temporal jump algorithm for clustering spatio-temporal data
+  # based on a distance matrix
+  
+  # Arguments:
+  # Y is a dataframe in long format with mandatory columns t and m which are T times and M spatial indexes
+  # n_states is the number of states
+  # D is the distance matrix of dimension MxM
+  # jump_penalty is the penalty for jumping between states
+  # spatial_penalty is the penalty for jumping between spatially close points
+  # initial_states is a matrix with the initial state of each point
+  # max_iter is the maximum number of iterations
+  # n_init is the number of initializations
+  # tol is the tolerance for stopping the algorithm
+  # verbose is a boolean for printing the loss at each iteration
+  # timeflag is a boolean, if TRUE the times are not equally sampled
+  
+  # Value:
+  # best_s is the best state sequence
+  # Y is the imputed data
+  # loss is the loss function at the optimum
+  
+  #
+  Y=Y[order(Y$t,Y$m),]
+  
+  P=ncol(Y)-2
+  # Time differences
+  Y.orig=Y
+  
+  if(timeflag){
+    time=sort(unique(Y.orig$t))
+    dtime=diff(time)
+    dtime=dtime/as.numeric(min(dtime))
+    dtime=as.numeric(dtime)
+    Y=subset(Y,select=-c(t,m))
+    Y=Y%>%mutate_if(is.numeric,function(x)as.numeric(scale(x)))
+  }
+  
+  else{
+    Y=subset(Y,select=-c(t,m))
+    Y=Y%>%mutate_if(is.numeric,function(x)as.numeric(scale(x)))
+  }
+  
+  Gamma <- jump_penalty * (1 - diag(n_states))
+  best_loss <- NULL
+  best_s <- NULL
+  
+  library(dplyr)
+  Y <- data.frame(t=Y.orig$t,m=Y.orig$m,Y)
+  YY=subset(Y,select=-c(t,m))
+  
+  TT=length(unique(Y$t))
+  M=length(unique(Y$m))
+  
+  cat.indx=which(sapply(YY, is.factor))
+  cont.indx=which(sapply(YY, is.numeric))
+  
+  Ycont=YY[,cont.indx]
+  
+  Ycat=YY[,cat.indx]
+  
+  n_cat=length(cat.indx)
+  n_cont=length(cont.indx)
+  
+  ###
+  # Missing data imputation TBD
+  # Track missings with 0 1 matrix
+  Mcont=ifelse(is.na(Ycont),T,F)
+  Mcat=ifelse(is.na(Ycat),T,F)
+  # Initialize mu 
+  mu <- colMeans(Ycont,na.rm = T)
+  # Initialize modes
+  mo <- apply(Ycat,2,Mode)
+  # Impute missing values with mean of observed states
+  for(i in 1:n_cont){
+    Ycont[,i]=ifelse(Mcont[,i],mu[i],Ycont[,i])
+  }
+  for(i in 1:n_cat){
+    x=Ycat[,i]
+    Ycat[which(is.na(Ycat[,i])),i]=mo[i]
+  }
+  YY[,-cat.indx]=Ycont
+  YY[,cat.indx]=Ycat
+  
+  Y[,-(1:2)]=YY
+  
+  ###
+  
+  # State initialization through kmeans++
+  S=matrix(0,nrow=TT,ncol=M)
+  for(m in 1:M){
+    S[,m]=initialize_states(Y[which(Y$m==m),-(1:2)],n_states)
   }
   
   for (init in 1:n_init) {
@@ -3960,7 +4171,7 @@ STjumpDist=function(Y,n_states,
     }
     
     for(m in 1:M){
-      S[,m]=initialize_states(Y[which(Y$m==m),],n_states)
+      S[,m]=initialize_states(Y[which(Y$m==m),-(1:2)],n_states)
     }
   }
   return(list(best_s=best_s,
