912529dd770c1a2f658393410d55de0b83b40a9e
Federico P. Cortese <cortese.federico@live.it>
2025-02-13 09:52:50 +0100

    fixed bug

--- R/SpJM/simstud_fuzzyJM/simstud_fuzzyJM.R
+++ R/SpJM/simstud_fuzzyJM/simstud_fuzzyJM.R
@@ -1,8 +1,8 @@
 library(parallel)
-library(snow)
-library(doSNOW)
+# library(snow)
+# library(doSNOW)
 
-source("Utils.R")
+#source("Utils.R")
 
 lambda=seq(0,1,by=.05)
 TT=c(50,100,500)
@@ -24,7 +24,29 @@ fuzzyJM_sim <- parallel::mclapply(1:nrow(hp),
                                         Pcat=NULL,pers=.95),
                                       mc.cores = parallel::detectCores()-1)
 
+# source("Utils_fuzzyJM.R")
+# cl<-makeCluster(parallel::detectCores(),type="SOCK")
+# parallel::clusterExport(cl,ls())
+# parallel::clusterEvalQ(cl,{
+#   library(cluster)
+#   library(StatMatch)
+# })
+# fuzzyJM_sim <- clusterApply(cl,
+#                             1:nrow(hp),
+#                             function(x)
+#                               simstud_fuzzyJM(seed=hp[x,]$seed,
+#                                               lambda=hp[x,]$lambda,
+#                                               TT=hp[x,]$TT,
+#                                               P=hp[x,]$P,
+#                                               K=3,mu=1,
+#                                               phi=.8,rho=0,
+#                                               Pcat=NULL,pers=.95)
+# )
+# stopCluster(cl)
+
 
 end_=Sys.time()
 elapsed_=end_-start_
 save(fuzzyJM_sim,elapsed_,file="fuzzyJM_sim.RData")
+
+
