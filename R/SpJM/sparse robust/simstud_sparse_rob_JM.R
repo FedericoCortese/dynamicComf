29cc6a146914856a288d8b9ff13db7c0b7005255
Federico P. Cortese <cortese.federico@live.it>
2025-07-07 12:03:51 +0200

    mods

--- R/SpJM/sparse robust/simstud_sparse_rob_JM.R
+++ R/SpJM/sparse robust/simstud_sparse_rob_JM.R
@@ -105,19 +105,7 @@ res_list_K3 <- mclapply(seq_len(nrow(hp)), function(i) {
   W_truth=simDat_sparse$W_truth
   ARI_s=mclust::adjustedRandIndex(est_s,truth)
   
-  ARI_W <- if (is.matrix(W_ind)) {
-    sapply(seq_len(ncol(W_ind)), function(j) {
-      tryCatch(
-        mclust::adjustedRandIndex(W_ind[, j], W_truth),
-        error = function(e) 0
-      )
-    })
-  } else {
-    tryCatch(
-      mclust::adjustedRandIndex(W_ind, W_truth),
-      error = function(e) 0
-    )
-  }
+  ARI_W <- mclust::adjustedRandIndex(W_ind, W_truth)
   
   res <- list(
     seed = seed,
